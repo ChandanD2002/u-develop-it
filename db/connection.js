3ced719f377afd984afa92ab48531c0cba835232
Manjeet <ChandanD@Chandans-MacBook-Air.local>
2021-10-27 04:26:42 -0700

    spring-cleaning

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Chandan2002',
+  database: 'election'
+});
+
+
+
+module.exports = db;
